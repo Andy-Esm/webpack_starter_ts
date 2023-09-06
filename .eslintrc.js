fa5b18f39e5c2612d73f3b42bdde8dae9d08083d
Andrey Esman <frontend@aesman.ru>
2023-09-06 20:25:32 +0300

    first version

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true
+  },
+  'extends': [
+    'plugin:jest/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      jsx: true
+    }
+  },
+  'plugins': [
+    'react'
+  ],
+  'settings': {
+    'react': {
+      'version': 'detect'
+    }
+  },
+  'rules': {
+    'semi': ['error', 'never'],
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'warn',
+    'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
+  }
+}
